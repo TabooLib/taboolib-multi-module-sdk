2dc39d44a62e64bc8cb6451fd9271d2dd8282125
坏黑 <b@skymc.me>
2023-02-06 20:42:55 +0800

    1

---
+++ project/core/src/main/kotlin/me/skymc/helloworld/HelloWorld.kt
@@ -0,0 +1,18 @@
+package me.skymc.helloworld
+
+import taboolib.module.configuration.Config
+import taboolib.module.configuration.Configuration
+
+/**
+ * 这是你插件对外开放的主类
+ * 可以开放一些接口
+ *
+ * 默认情况下这个模块不包含 Bukkit 核心
+ * 如果你的插件没有跨平台需求，可以在这个项目中引入 Bukkit 核心，以使用 Bukkit 的 API
+ */
+object HelloWorld {
+
+    @Config
+    lateinit var conf: Configuration
+        private set
+}
