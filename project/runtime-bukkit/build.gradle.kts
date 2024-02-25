acb801798c48a9a4501483bb4cf648fd5340a6aa
Bkm016 <sky@asgard.me>
2024-02-25 18:21:20 +0800

    更新版本

--- project/runtime-bukkit/build.gradle.kts
+++ project/runtime-bukkit/build.gradle.kts
@@ -4,8 +4,8 @@ dependencies {
     // 引入 API
     compileOnly(project(":project:core"))
     // 引入 服务端核心
-    compileOnly("ink.ptms.core:v11903:11903:mapped")
-    compileOnly("ink.ptms.core:v11903:11903:universal")
+    compileOnly("ink.ptms.core:v12004:12004:mapped")
+    compileOnly("ink.ptms.core:v12004:12004:universal")
 }
 
 // 子模块
