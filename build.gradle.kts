9aa371d8472be78c39e41a25d42c88617983f7a9
黑 <b@skymc.me>
2024-02-28 18:29:18 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     java
-    id("io.izzel.taboolib") version "2.0.7"
+    id("io.izzel.taboolib") version "2.0.9"
     id("org.jetbrains.kotlin.jvm") version "1.8.22"
 }
 
