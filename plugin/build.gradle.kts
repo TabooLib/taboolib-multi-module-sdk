4f17a5f26461a7d6086c2454c5889df21f95939b
Bkm016 <sky@asgard.me>
2024-03-22 04:22:01 +0800

    更新版本

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -15,13 +15,13 @@ taboolib {
 tasks {
     jar {
         // 构件名
-        archiveFileName.set("${rootProject.name}-${archiveFileName.get().substringAfter('-')}")
+        archiveBaseName.set(rootProject.name)
         // 打包子项目源代码
         rootProject.subprojects.forEach { from(it.sourceSets["main"].output) }
     }
     sourcesJar {
         // 构件名
-        archiveFileName.set("${rootProject.name}-${archiveFileName.get().substringAfter('-')}")
+        archiveBaseName.set(rootProject.name)
         // 打包子项目源代码
         rootProject.subprojects.forEach { from(it.sourceSets["main"].allSource) }
     }
